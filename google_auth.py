bb7f2da87edf30d21ae6e6db71d519358e94a255
vaishnavi <vaishnavi.sara25@gmail.com>
2025-03-17 20:54:35 +0530

    firstCommit

--- google_auth.py
+++ google_auth.py
@@ -10,7 +10,7 @@ from flask_login import login_required, login_user, logout_user
 from models import User
 from oauthlib.oauth2 import WebApplicationClient
 
-GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_OAUTH_CLIENT_ID", "")
+GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_OAUTH_CLIENT_ID", "579014633235-hnrkip7bnfdrsne6eu707qm2tl5lq836.apps.googleusercontent.com")
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_OAUTH_CLIENT_SECRET", "")
 GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
 
